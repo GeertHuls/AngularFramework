3c4bb4d514fb65f4a72febd0667bf60fc7d5ee12
Geert Huls <geert.huls82@gmail.com>
2017-05-04 21:09:28 +0200

    Menu orientation toggle

--- src/fw/services/menu.service.ts
+++ src/fw/services/menu.service.ts
@@ -17,4 +17,7 @@ export class MenuService {
     this.isVertical = true;
     this.showingLeftSideMenu = !this.showingLeftSideMenu;
   }
+  toggleMenuOrientation() {
+    this.isVertical = !this.isVertical;
+  }
 }
