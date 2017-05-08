0248a40e5420cad2009cbfd7942fbd0f5ec6782e
Geert Huls <geert.huls82@gmail.com>
2017-05-08 07:00:31 +0200

    User service abstract base class and implementation

---
+++ src/app/Services/user-service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Observable } from 'rxjs/Rx';
+import { UserApi } from '../../fw/users/user-api';
+
+@Injectable()
+export class UserService implements UserApi {
+
+  isAuthenticated = true;
+
+  constructor() { }
+
+  signIn(username: string, password: string, rememberMe: boolean): Observable<any> {
+    console.log('UserService.signIn: ' + username + ' ' + password + ' ' + rememberMe);
+    this.isAuthenticated = true;
+    return Observable.of({});
+  }
+}
